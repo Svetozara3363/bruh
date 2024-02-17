e097e3655bc573ceb982aee0c7d844654d79c24b
Svetozara3363 <120119368+Svetozara3363@users.noreply.github.com>
2024-02-17 19:25:06 +0300

    Initial Commit

---
+++ bruh/bruhApp.swift
@@ -0,0 +1,17 @@
+//
+//  bruhApp.swift
+//  bruh
+//
+//  Created by svetozara on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct bruhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
